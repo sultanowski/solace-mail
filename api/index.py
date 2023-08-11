177673c02caf1c4cb8df74470ac208abba3638d4
bartmkraus <bartmkraus@gmail.com>
2023-08-11 15:51:53 +0200

    feat(api)!: install fastapi users and add boiler plate code

--- api/index.py
+++ api/index.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
+
 
 @app.get("/api/python")
 def hello_world():
     return {"message": "Hello World"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("auth.router:app", host="0.0.0.0", log_level="info")
